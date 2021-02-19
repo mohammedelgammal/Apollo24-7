a29222449260cfbfb5721ba562bb4a87e289ddb4
someone <someone@someemail.com>
2021-02-19 20:17:20 +0200

    modified footer and doctors addition

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,38 @@ let swiperTop = new Swiper('.top', {
     freeMode: true,
     centeredSlides: true
 });
+
+let items = document.querySelectorAll('footer .child h3');
+let spanContainer = document.querySelectorAll('footer .child .span-container');
+
+for(let i = 0; i < items.length; i++){
+    items[i].addEventListener('click', () => {
+        if(spanContainer[i].style.display === 'flex'){
+            spanContainer[i].style.display = 'none';
+        } else {
+            spanContainer[i].style.display = 'flex';
+        }
+    })
+}
+
+let doctor = document.querySelector('.doctor img');
+let closingPanel = document.querySelector('.closing');
+let docPanel = document.querySelector('.doctor-panel');
+
+closingPanel.addEventListener('click', () => {
+    docPanel.style.display = 'none';
+    closingPanel.style.display = 'none';
+});
+
+doctor.addEventListener('click', () => {
+    if(docPanel.style.display === 'flex'){
+        docPanel.style.display = 'none';
+        closingPanel.style.display = 'none';
+    } else {
+        docPanel.style.display = 'flex';
+        closingPanel.style.display = 'flex';
+    }
+});
+
+
+
